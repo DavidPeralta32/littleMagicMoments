4e657b6464238c0e0f6bebde9696340216039613
David Peralta <davi32peralta@gmail.com>
2021-11-26 09:20:39 -0600

    index.php terminada , solo falta darle estilos

---
+++ views/xvView.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>XV Años</title>
+</head>
+<body>
+    <h1>Aqui van los fotos de XV años</h1>
+</body>
+</html>
